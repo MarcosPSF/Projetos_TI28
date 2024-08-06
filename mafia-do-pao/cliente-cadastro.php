31954418180fa12acc9054cf1f5842b57085d54d
MarcosPSF <framarco611@gmail.com>
2024-08-06 09:55:36 -0300

    atulização do projeto mafia do pão
    atualização mafia do pão

---
+++ mafia-do-pao/cliente-cadastro.php
@@ -0,0 +1,66 @@
+<?php
+include("conectadb.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $cpf = $_POST['txtcpf'];
+    $nome = $_POST['txtnome'];
+    $email = $_POST['txtemail'];
+    $cel = $_POST['txtcel'];
+
+// VALIDA SE CLIENTE A CADASTRAR EXISTE
+    $sql = "SELECT COUNT(cli_id) FROM tb_clientes WHERE cli_cpf = '$cpf' ";
+
+ // RETORNO DO BANCO
+    $retorno = mysqli_query($link, $sql);
+    $contagem = mysqli_fetch_array($retorno) [0];
+
+// VERIFICA SE CLIENTE EXISTE
+    if($contagem == 0){
+        $sql = "INSERT INTO tb_clientes(cli_cpf, cli_nome, cli_email, cli_cel, cli_status)
+        VALUES ('$cpf', '$nome', '$email', '$cel', '1')";
+        mysqli_query($link, $sql);
+        echo"<script>window.alert('CLIENTE CADASTRADO COM SUCESSO');</script>";
+        echo"<script>window.location.href='backoffice.php';</script>";
+    }
+    else if($contagem >= 1){
+        echo"<script>window.alert('CLIENTE JÁ EXISTENTE');</script>";
+    }
+}       
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link href="https://fonts.cdnfonts.com/css/curely" rel="stylesheet">
+    <title>Cadastro cliente</title>
+</head>
+<body>
+<a href="backoffice.php"><img src="icons/Navigation-left-01-256.png" width="25" height="25"></a>
+<div class="container-global">
+
+    <form class="formulario" action="cliente-cadastro.php" method="post">
+        <label>CPF</label>
+        <input type="text" id="cpf" name="txtcpf" placeholder="000.000.000-00" maxlength="14" oninput="formatarCPF(this)">
+        <br>
+        <label>DIGITE SEU NOME</label>
+        <input type="text" name="txtnome" placeholder="Digote seu Nome" required>
+        <br>
+        <label>EMAIL</label>
+        <input type="email" name="txtemail" placeholder="Digite seu email" required>
+        <br>
+        <label>NUMERO DE CELULAR</label>
+        <input type="text" name="txtcel" id="telefone"placeholder="(00) 00000-0000"maxlength="15">
+
+        <br>
+        <input type="submit" value="CRIAR">
+    </form>
+
+</div>
+
+<script src="scripts/script.js"></script>
+
+</body>
+</html>
--- mafia-do-pao/login.php
+++ mafia-do-pao/login.php
@@ -1,52 +1,67 @@
 <?php
-    include("conectadb.php");
+session_start();
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $login = $_POST['txtlogin'];
-        $senha = $_POST['txtsenha'];
+include("conectadb.php");
 
-        //COMEÇA A VAIDAR BANCO DE DADOS
-        $sql = "SELECT COUNT(usu_id) FROM tb_usuario WHERE usu_login = '$login' AND usu_senha = '$senha' AND usu_status = '1'"; 
-        
-        //RETORNO DO BANCO
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $login = $_POST['txtlogin'];
+    $senha = $_POST['txtsenha'];
+
+    // COMEÇA VALIDAR BANCO DE DADOS
+    $sql = "SELECT COUNT(usu_id) FROM tb_usuarios
+    WHERE usu_login = '$login' AND usu_senha = '$senha' AND
+    usu_status = '1'";
+    // RETORNO DO BANCO
+    $retorno = mysqli_query($link, $sql);
+
+    $contagem = mysqli_fetch_array($retorno) [0];
+
+    // VERIFICA SE NATAN EXISTE
+    if($contagem == 1){
+        $sql = "SELECT usu_id, usu_login FROM tb_usuarios
+        WHERE usu_login = '$login'AND usu_senha = '$senha'";
         $retorno = mysqli_query($link, $sql);
-
+        //RETORNANDO O NOME DO NATAN + ID DELE
         while($tbl = mysqli_fetch_array($retorno)){
-            $contagem = $tbl[0];
+            $_SESSION['idusuario'] = $tbl[0];
+            $_SESSION['nomeusuario'] = $tbl[1];
         }
-        //VERIFICAR SE O NATAN EXISTE 
-        if($contagem == 1){
-            echo"<script>window.location.href='home.php';</script>";
-        }
-        else{
-            echo"<script>window.alert('USUARIO OU SENHA INCORRETO');</script>";
-        }
+        echo"<script>window.location.href='backoffice.php';</script>";
     }
+    else{
+        echo"<script>window.alert('USUARIO OU SENHA INCORRETOS');</script>";
+    }
+
+}
 ?>
 
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilo.css">
+    <link href="https://fonts.cdnfonts.com/css/curely" rel="stylesheet">
+                
     <title>LOGIN USUARIO</title>
 </head>
 <body>
     <div class="container-global">
-        <form class="formulario" action="login.php" method="post">
-        <img src="img/logo.png" width=100 height=100>
-            <label>LOGIN</label>
-            <br>
-            <input type="text" name="txtlogin" required>
-            <br>
-            <label>SENHA</label>
-            <br>
-            <input type="password" name="txtsenha" required>
-            <br>
-            <br>
-            <input type="submit" value="ACESSAR">
+    
+
+    <form class="formulario" action="login.php" method="post">
+        <img src="img/logo.jfif" width="50" height="50">
+                <label>LOGIN</label>
+                <input type="text" name="txtlogin" placeholder="Digite seu login" required>
+                <br>
+                <label>SENHA</label>
+                <input type="password" name="txtsenha" placeholder="Digite sua senha" required>
+                <br>
+                <br>
+                <input type="submit" value="ACESSAR">
         </form>
+
     </div>
+    
 </body>
 </html>
